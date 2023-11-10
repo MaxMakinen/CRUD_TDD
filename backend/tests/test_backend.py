12c5e8b77bd39c29f9b6c5cc4e2c387adde0f038
Max Mäkinen <quantumhound@gmail.com>
2023-11-10 15:34:04 +0200

    commented out useless tests

--- backend/tests/test_backend.py
+++ backend/tests/test_backend.py
@@ -5,16 +5,16 @@ from backend.app import app, db
 from sqlalchemy import inspect
 from backend.models import Item
 
-database_path = '../database/tasks.db'
-table_name = 'tasks'
-
-
-def test_database_presence():
-    assert is_database_present(database_path)
-
-def test_database_table_presence():
-    assert is_table_present(database_path, table_name)
-
+# database_path = '../database/database.sqlite'
+# table_name = 'mlem'
+# 
+# 
+# def test_database_presence():
+    # assert is_database_present(database_path)
+# 
+# def test_database_table_presence():
+    # assert is_table_present(database_path, table_name)
+# 
 def test_items_table_exists():
     with app.app_context():
         inspector = inspect(db.engine)
